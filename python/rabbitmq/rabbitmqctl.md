60b895d3d3994045bd2cf8a28c3ee11c4d6ae5d3
hudson <hudson.wu@fairhr.com>
2020-03-02 18:04:54 +0800

    work queue

--- python/rabbitmq/rabbitmqctl.md
+++
@@ -1,6 +0,0 @@
-# rabbitmqctl命令
-
-```
-# 查看rabbitmq有多少队列(queues)以及每个队列中的消息数(messages)
-rabbitmqctl list_queues
-```
--- python/rabbitmq/workqueue/worker.py
+++ python/rabbitmq/workqueue/worker.py
@@ -15,17 +15,20 @@ connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
 # 创建一个queue
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 # 定义callback函数
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+# 公平调度
+channel.basic_qos(prefetch_count=1)
 
 # 接收消息
-channel.basic_consume(queue='hello',
-                      auto_ack=True,
+channel.basic_consume(queue='task_queue',
                       on_message_callback=callback)
 
 # 等待接收消息

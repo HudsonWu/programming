fcb4879771d9684dcef5b6b40b2b6c14489b449e
hudson <wuhaishui2013@126.com>
2020-06-22 08:37:30 +0800

    组件的更新

--- vue/single-file_components/vue-demo/src/App.vue
+++ vue/single-file_components/vue-demo/src/App.vue
@@ -1,48 +1,46 @@
 <template>
-  <div id="app">
-    {{message}} {{message + " " + message}}
-    <!-- <div v-bind:id="message"></div> -->
-    <!-- 使用v-bind指令可以渲染动态值，v-bind:，可以简写为: -->
-    <div :id="message"></div>
-    <todo-list>
-      <todo-item @delete="handleDelete" v-for="item in list" :key="item.title" :title="item.title" :del="item.del">
-        <template v-slot:pre-icon="{value}">
-          <slot>前置图标 {{value}}</slot>
-        </template>
-        <template v-slot:suf-icon>
-          <slot>后置图标</slot>
-        </template>
-      </todo-item>
-    </todo-list>
+  <div>
+    <p>
+      <button @click="handleNameChange">change this.name</button>
+      <button @click="handleInfoChange">change this.info</button>
+      <button @click="handleListChange">change this.list</button>
+    </p>
+    <PropsAndData :name="name" :info="info" :list="list" />
   </div>
 </template>
-
 <script>
-import TodoList from './components/TodoList.vue'
-import TodoItem from './components/TodoItem.vue'
-
+import PropsAndData from "./components/PropsAndData";
+let name = "world";
 export default {
-  name: 'App',
   components: {
-    TodoList,
-    TodoItem
+    PropsAndData
   },
   data() {
+    this.name = name;
     return {
-      message: 'hello world',
-      list: [{
-        title: '课程1',
-        del: false
-      }, {
-        title: '课程2',
-        del: true
-      }],
-    }
+      info: {},
+      // name: name,
+      // info: {
+      //   number: undefined
+      // },
+      list: []
+    };
   },
   methods: {
-    handleDelete(val) {
-      console.log('handleDelete', val)
+    handleNameChange() {
+      this.name = "vue" + Date.now();
+      console.log("this.name 发生了变化，但是并没有触发子组件更新", this.name);
+    },
+    handleInfoChange() {
+      this.info.number = 1;
+      // this.$set(this.info, 'number', 1)
+      console.log("this.info 发生了变化，但是并没有触发子组件更新", this.info);
+    },
+    handleListChange() {
+      this.list.push(1, 2, 3);
+      //并不会改变this.list的引用，只是改变了数组的内容
+      console.log("this.list 并没有发生变化，但是触发了子组件更新", this.list);
     }
   }
-}
+};
 </script>
